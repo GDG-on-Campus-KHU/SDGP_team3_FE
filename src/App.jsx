01caf7a5c99d3d59c8343b5249d125864469ce34
youngju6143 <youngju6143@naver.com>
2025-04-23 17:07:41 +0900

    📦️ Chore: tailwindcss 초기 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-dvh w-full justify-center">
+      <div className="flex w-full max-w-[600px] flex-col bg-background">
+        <p className="w-6 h-6 bg-red-500 text-red">fadsfasd</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
