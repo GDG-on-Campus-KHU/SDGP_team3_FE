0eb670e2db931fe668e218b1429ace50790ceaa4
youngju6143 <youngju6143@naver.com>
2025-05-02 16:58:47 +0900

    Merge feat/#9 to feat/#8

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import { Btn } from "./shared/ui/button";
-import Header from "./shared/ui/header";
-import Main from "./pages/main";
+import MainPage from "./pages/main";
+import ChallengeCard from "./shared/ui/ChallengeCard";
+import { CHALLENGE_MOCK_DATAS_1 } from "./shared/config/challengeConfig";
+import { Checkbox } from "./shared/ui/checkbox";
 
 function App() {
   return (
     <div className="flex h-dvh w-full justify-center">
       <div className="relative flex w-full max-w-[600px] flex-col bg-background">
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/create" element={<p>챌린지 생성 페이지</p>} />
-          <Route path="/world" element={<p>나의 세계 페이지</p>} />
+          <Route path="/" element={<MainPage />} />
+          <Route
+            path="/create"
+            element={<ChallengeCard challenge={CHALLENGE_MOCK_DATAS_1[2]} />}
+          />
+          <Route path="/world" element={<Checkbox />} />
         </Routes>
       </div>
     </div>
