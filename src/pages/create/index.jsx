8e412851a2feb3d0e3bcd80599afaef6792e9b25
영주 <111039206+youngju6143@users.noreply.github.com>
2025-05-08 01:58:33 +0900

    [FEAT] 챌린지 생성하기 UI 구현 (#16)
    * 💄 Style: 디자인 시스템 수정, 모달 최대 너비 적용
    
    * ✨ Feat: 챌린지 생성 폼 제작
    
    * ♻️ Refactor: 헤더 내용 config로 상수화, 변수명 수정

---
+++ src/pages/create/index.jsx
@@ -0,0 +1,6 @@
+import Create from "@/features/create";
+import React from "react";
+
+export default function CreatePage() {
+  return <Create />;
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,158 +3,140 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		fontSize: {
-  			'title-01': [
-  				'20px',
-  				{
-  					lineHeight: '140%',
-  					letterSpacing: '-0.005em',
-  					fontWeight: 700
-  				}
-  			],
-  			'title-02': [
-  				'18px',
-  				{
-  					lineHeight: '140%',
-  					letterSpacing: '-0.005em',
-  					fontWeight: 700
-  				}
-  			],
-  			'body-01': [
-  				'16px',
-  				{
-  					lineHeight: '150%',
-  					letterSpacing: '0em',
-  					fontWeight: 600
-  				}
-  			],
-  			'body-02': [
-  				'14px',
-  				{
-  					lineHeight: '150%',
-  					letterSpacing: '0em',
-  					fontWeight: 600
-  				}
-  			],
-  			'body-03': [
-  				'12px',
-  				{
-  					lineHeight: '150%',
-  					letterSpacing: '0em',
-  					fontWeight: 500
-  				}
-  			],
-  			'caption-01': [
-  				'10px',
-  				{
-  					lineHeight: '140%',
-  					letterSpacing: '0.2px',
-  					fontWeight: 500
-  				}
-  			],
-  			'caption-02': [
-  				'8px',
-  				{
-  					lineHeight: '140%',
-  					letterSpacing: '0.2px',
-  					fontWeight: 500
-  				}
-  			]
-  		},
-  		fontFamily: {
-  			pretendard: [
-  				'Pretendard',
-  				'sans-serif'
-  			]
-  		},
-  		fontWeight: {
-  			medium: '500',
-  			semibold: '600',
-  			bold: '700'
-  		},
-  		colors: {
-  			black: '#222222',
-  			white: '#FDFDFD',
-  			background: 'hsl(var(--background))',
-  			gray: {
-  				'100': '#E2E2E2',
-  				'300': '#CBCBCB',
-  				'500': '#9C9C9C',
-  				'700': '#787878'
-  			},
-  			green: {
-  				'300': '#A7E8C2',
-  				'500': '#32BC81'
-  			},
-  			red: {
-  				'300': '#F6A6A6',
-  				'500': '#F26C63'
-  			},
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		boxShadow: {
-  			md: '0 2px 2px rgba(0, 0, 0, 0.25)'
-  		},
-  		zIndex: {
-  			bar: 200,
-  			modalBackground: 300,
-  			modal: 400,
-  			toast: 500
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	},
-  	fontFamily: {
-  		sans: [
-  			'Pretendard',
-  			'sans-serif'
-  		]
-  	}
+    extend: {
+      fontSize: {
+        "title-01": [
+          "20px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "-0.005em",
+            fontWeight: 700,
+          },
+        ],
+        "title-02": [
+          "18px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "-0.005em",
+            fontWeight: 700,
+          },
+        ],
+        "body-01": [
+          "16px",
+          {
+            lineHeight: "150%",
+            letterSpacing: "0em",
+            fontWeight: 600,
+          },
+        ],
+        "body-02": [
+          "14px",
+          {
+            lineHeight: "150%",
+            letterSpacing: "0em",
+            fontWeight: 600,
+          },
+        ],
+        "body-03": [
+          "12px",
+          {
+            lineHeight: "150%",
+            letterSpacing: "0em",
+            fontWeight: 500,
+          },
+        ],
+        "caption-01": [
+          "10px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "0.2px",
+            fontWeight: 500,
+          },
+        ],
+        "caption-02": [
+          "8px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "0.2px",
+            fontWeight: 500,
+          },
+        ],
+      },
+      fontFamily: {
+        pretendard: ["Pretendard", "sans-serif"],
+      },
+      fontWeight: {
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+      },
+      colors: {
+        black: "#222222",
+        white: "#FDFDFD",
+        background: "#F8F8F8",
+        gray: {
+          100: "#E2E2E2",
+          300: "#CBCBCB",
+          500: "#9C9C9C",
+          700: "#787878",
+        },
+        green: {
+          300: "#A7E8C2",
+          500: "#32BC81",
+        },
+        red: {
+          300: "#F6A6A6",
+          500: "#F26C63",
+        },
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+      },
+      boxShadow: {
+        md: "0 2px 2px rgba(0, 0, 0, 0.25)",
+      },
+      zIndex: {
+        bar: 200,
+        modalBackground: 300,
+        modal: 400,
+        toast: 500,
+      },
+    },
+    fontFamily: {
+      sans: ["Pretendard", "sans-serif"],
+    },
   },
   plugins: [require("tailwindcss-animate")],
 });
