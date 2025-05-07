a46927925abba32c1be06568a66267984d67ff0d
youngju6143 <youngju6143@naver.com>
2025-05-08 04:44:48 +0900

    ✨ Feat: 챌린지 조회 API 예외처리 추가

--- src/features/main/model/api/challengeApi.js
+++ src/features/main/model/api/challengeApi.js
@@ -4,6 +4,15 @@ const SERVER_URL = import.meta.env.VITE_APP_SERVER_URL;
 const BASE_URL = "/challenges";
 
 export const getChallenges = async () => {
-  const response = await axios.get(`${SERVER_URL}${BASE_URL}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${SERVER_URL}${BASE_URL}`, {
+      headers: {
+        Authorization: `Bearer Token넣기`,
+      },
+    });
+    console.log("챌린지 조회 성공! : ", response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
